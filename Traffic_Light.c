d06b191a3c237ff04c367ced684187f1c9e6ef24
Hilquias Rodrigues de Oliveira <eaeamigo2292@gmail.com>
2025-05-03 17:45:59 -0300

    Interaction between vehicle and pedestrian traffic lights

--- Traffic_Light.c
+++ Traffic_Light.c
@@ -10,6 +10,7 @@
 #include "Led_Matrix.h" // Inclusão da biblioteca para controlar a matriz de LEDs>
 #include "Buzzer.h"     // Inclusão da biblioteca para controlar o buzzer
 #include "Button.h"     // Inclusão da biblioteca para controlar os botões
+#include "Display.h"    // Inclusão da biblioteca para controlar o display OLED
 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
@@ -58,18 +59,13 @@ void vTrafficLightMatrixTask()
 // Tarefa para controlar o buzzer
 void vTrafficLightBuzzerTask()
 {
-    semaphore_state current_state;
-
     while (true)
     {
-        // Lê o estado atual da variável global
-        current_state = g_current_state;
-
         // Se estiver no modo noturno, só o LED amarelo deve piscar levemente a cada 2 segundos
         if (night_mode)
         {
             // Em modo noturno, o LED amarelo pisca levemente
-            if (current_state == YELLOW)
+            if (g_current_state == YELLOW)
             {
                 set_buzzer_level(BUZZER_A, WRAP_PWM_BUZZER / 90); // Buzzer baixo para piscar levemente
                 vTaskDelay(pdMS_TO_TICKS(1000));                  // Espera 700ms
@@ -80,7 +76,7 @@ void vTrafficLightBuzzerTask()
         }
 
         // Processa o estado e gera o som apropriado
-        switch (current_state)
+        switch (g_current_state)
         {
         case RED:
             // Tom contínuo curto (500ms ligado e 1.5s desligado) “pare”
@@ -120,72 +116,23 @@ void vTrafficLightBuzzerTask()
 // Tarefa para controlar o display OLED
 void vDisplay3Task()
 {
-    i2c_init(I2C_PORT, 400 * 1000); // Inicializa o I2C na porta I2C_PORT com 400kHz
-
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Configura o pino SDA para I2C
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Configura o pino SCL para I2C
-    gpio_pull_up(I2C_SDA);                                        // Habilita o pull-up interno para SDA
-    gpio_pull_up(I2C_SCL);                                        // Habilita o pull-up interno para SCL
-    ssd1306_t ssd;                                                // Inicializa a estrutura do display
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-    ssd1306_config(&ssd);                                         // Configura o display
-    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
-
-    // Limpa o display. O display inicia com todos os pixels apagados.
-    ssd1306_fill(&ssd, false);
-    ssd1306_send_data(&ssd);
-
-    bool cor = true;
     while (true)
     {
-        // ssd1306_fill(&ssd, !cor);                            // Limpa o display
-        // ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);        // Desenha um retângulo
-        // ssd1306_line(&ssd, 20, 37, 54, 8, cor);             // Desenha uma linha
-        // ssd1306_send_data(&ssd);                             // Atualiza o display
-
-        ssd1306_fill(&ssd, false); // Limpa o display
-        ssd1306_rect(&ssd, 2, 34, 61, 62, cor, !cor); 
-
-        // Cabeça
-        ssd1306_rect(&ssd, 8, 59, 10, 10, cor, !cor); 
-        
-        // Ombro
-        ssd1306_line(&ssd, 58, 20, 70, 20, true);
-
-        // Corpo (linha)
-        ssd1306_vline(&ssd, 58, 20, 36, true);
-        ssd1306_vline(&ssd, 70, 26, 36, true);
-
-
-        // Braço esquerdo
-        ssd1306_line(&ssd, 58, 26, 50, 32, true);
-        ssd1306_line(&ssd, 50, 32, 46, 32, true);
-        ssd1306_line(&ssd, 46, 32, 44, 31, true);
-        
-        ssd1306_line(&ssd, 58, 20, 50, 28, true);
-        ssd1306_line(&ssd, 50, 28, 46, 28, true);
-        ssd1306_line(&ssd, 46, 28, 44, 30, true);
-        
-        // Braço direito
-        ssd1306_line(&ssd, 70, 26, 80, 34, true);
-        ssd1306_line(&ssd, 70, 20, 82, 30, true);
-        ssd1306_line(&ssd, 82, 30, 83, 33, true);
-        ssd1306_line(&ssd, 81, 34, 83, 33, true);
-
-        // Perna esquerda
-        ssd1306_line(&ssd, 58, 36, 44, 58, true);
-        ssd1306_line(&ssd, 64, 40, 52, 58, true);
-        ssd1306_line(&ssd, 44, 58, 52, 58, true);
-        
-        // Perna direita
-        ssd1306_line(&ssd, 64, 40, 76, 58, true);
-        ssd1306_line(&ssd, 70, 36, 84, 58, true);
-        ssd1306_line(&ssd, 76, 58, 84, 58, true);
-
-        // Atualiza o display
-        ssd1306_send_data(&ssd);
-
-        sleep_ms(735);
+        switch (g_current_state)
+        {
+        case RED:
+            draw_walking_pedestrian(); // Desenha o pedestre caminhando
+            break;
+        case YELLOW:
+            draw_standing_pedestrian(); // Desenha o pedestre parado
+            break;
+        case GREEN:
+            draw_standing_pedestrian(); // Desenha o pedestre parado
+            break;
+        default:
+            break;
+        }
+        vTaskDelay(pdMS_TO_TICKS(250));  
     }
 }
 
@@ -230,10 +177,10 @@ int main()
     gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     // Fim do trecho para modo BOOTSEL com botão B
 
-    configure_leds_matrix();    // Configura a matriz de LEDs
-    configure_buzzer();         // Configura o buzzer
+    configure_leds_matrix(); // Configura a matriz de LEDs
+    configure_buzzer();      // Configura o buzzer
     // configure_button(BUTTON_A); // Configura o botão A
-    // configure_button(BUTTON_B); // Configura o botão B
+    configure_i2c_display(); // Configura o display I2C
 
     xTaskCreate(vTrafficLightMatrixTask, "Traffic Light Matrix Task", configMINIMAL_STACK_SIZE,
                 NULL, tskIDLE_PRIORITY, NULL);
---
+++ src/Display.c
@@ -0,0 +1,162 @@
+#include "Display.h" // Inclusão do cabeçalho com definições relacionadas ao display OLED
+
+ssd1306_t ssd;   // Estrutura que representa o display OLED SSD1306
+bool cor = true; // Variável para armazenar a cor do display (true ou false)
+
+// Função para configurar a comunicação I2C e inicializar o display OLED
+void configure_i2c_display()
+{
+    // Inicializa o barramento I2C na porta e com frequência de 400 kHz
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    // Define as funções dos pinos SDA e SCL como I2C
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+
+    // Ativa resistores de pull-up nos pinos SDA e SCL
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    // Inicializa o display com resolução 128x64, sem rotação, endereço I2C e porta definida
+    ssd1306_init(&ssd, 128, 64, false, DISPLAY_ADDRESS, I2C_PORT);
+
+    // Configura o display com parâmetros padrão
+    ssd1306_config(&ssd);
+
+    // Envia os dados iniciais para o display
+    ssd1306_send_data(&ssd);
+}
+
+// Função para limpar o display OLED
+void clear_i2c_display()
+{
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+}
+
+void draw_walking_pedestrian()
+{
+
+    ssd1306_fill(&ssd, false);                    // Limpa o display antes de desenhar o pedestre
+    ssd1306_rect(&ssd, 2, 34, 61, 62, cor, !cor); // Desenha um retângulo respresntando a placa
+
+    // Cabeça
+    ssd1306_hline(&ssd, 62, 64, 9, cor); // topo
+
+    // face esquerda
+    ssd1306_line(&ssd, 62, 9, 59, 12, cor); 
+    ssd1306_vline(&ssd, 59, 12, 15, cor); 
+    ssd1306_line(&ssd, 59, 15, 62, 18, cor);
+
+    // face direita
+    ssd1306_line(&ssd, 65, 9, 67, 12, cor); 
+    ssd1306_vline(&ssd, 67, 12, 15, cor); 
+    ssd1306_line(&ssd, 67, 15, 65, 18, cor); 
+    
+    ssd1306_hline(&ssd, 62, 64, 18, cor); // queixo
+
+    // Ombro
+    ssd1306_line(&ssd, 58, 20, 70, 20, true);
+
+    // Tronco
+    ssd1306_vline(&ssd, 58, 20, 36, true);
+    ssd1306_vline(&ssd, 70, 26, 36, true);
+
+    // Braço esquerdo
+    ssd1306_line(&ssd, 58, 26, 50, 32, true);
+    ssd1306_line(&ssd, 50, 32, 46, 32, true);
+    ssd1306_line(&ssd, 46, 32, 44, 31, true);
+
+    ssd1306_line(&ssd, 58, 20, 50, 28, true);
+    ssd1306_line(&ssd, 50, 28, 46, 28, true);
+    ssd1306_line(&ssd, 46, 28, 44, 30, true);
+
+    // Braço direito
+    ssd1306_line(&ssd, 70, 26, 80, 34, true);
+    ssd1306_line(&ssd, 70, 20, 82, 30, true);
+    ssd1306_line(&ssd, 82, 30, 83, 33, true);
+    ssd1306_line(&ssd, 81, 34, 83, 33, true);
+
+    // Perna esquerda
+    ssd1306_line(&ssd, 58, 36, 44, 54, true);
+    ssd1306_line(&ssd, 64, 40, 50, 58, true);
+    
+    ssd1306_line(&ssd, 44, 55, 45, 58, true); 
+    ssd1306_hline(&ssd, 46, 49, 58, true); // pé
+    
+
+    // Perna direita
+    ssd1306_line(&ssd, 64, 40, 76, 58, true);
+    ssd1306_line(&ssd, 70, 36, 82, 54, true);
+
+    ssd1306_line(&ssd, 82, 55, 80, 58, true); 
+    ssd1306_hline(&ssd, 77, 79, 58, true); // pé
+
+    // Atualiza o display
+    ssd1306_send_data(&ssd);
+}
+
+void draw_standing_pedestrian()
+{
+    
+    ssd1306_fill(&ssd, false);                    // Limpa o display antes de desenhar o pedestre
+    ssd1306_rect(&ssd, 2, 34, 61, 62, cor, !cor); // Desenha um retângulo respresntando a placa
+
+    // Cabeça
+    ssd1306_hline(&ssd, 62, 66, 9, cor); // topo
+
+    // face esquerda
+    ssd1306_line(&ssd, 62, 9, 59, 12, cor); 
+    ssd1306_vline(&ssd, 59, 12, 15, cor); 
+    ssd1306_line(&ssd, 59, 15, 62, 18, cor);
+
+    // face direita
+    ssd1306_line(&ssd, 66, 9, 69, 12, cor); 
+    ssd1306_vline(&ssd, 69, 12, 15, cor); 
+    ssd1306_line(&ssd, 69, 15, 66, 18, cor); 
+    
+    ssd1306_hline(&ssd, 62, 66, 18, cor); // queixo
+
+    // Ombro
+    ssd1306_line(&ssd, 58, 20, 70, 20, true);
+
+    // Tronco
+    ssd1306_vline(&ssd, 58, 26, 36, true);
+    ssd1306_vline(&ssd, 70, 26, 36, true);
+
+    // Braço esquerdo
+    ssd1306_line(&ssd, 58, 20, 46, 30, true); // cima
+    ssd1306_line(&ssd, 58, 26, 48, 34, true); // baixo
+
+    ssd1306_line(&ssd, 46, 30, 45, 33, true); // mão cima
+    ssd1306_line(&ssd, 47, 34, 45, 33, true); // mão baixo
+
+    // Braço direito
+    ssd1306_line(&ssd, 70, 20, 82, 30, true); // cima
+    ssd1306_line(&ssd, 70, 26, 80, 34, true); // baixo
+
+    ssd1306_line(&ssd, 82, 30, 83, 33, true); // mão cima
+    ssd1306_line(&ssd, 81, 34, 83, 33, true); // mão baixo
+
+    // Perna esquerda
+    ssd1306_line(&ssd, 58, 36, 52, 54, true); // fora
+    ssd1306_line(&ssd, 64, 40, 60, 54, true); // dentro
+
+    ssd1306_line(&ssd, 52, 55, 54, 58, true); 
+    ssd1306_line(&ssd, 60, 55, 58, 58, true); 
+
+    ssd1306_hline(&ssd, 55, 57, 58, true); // pé
+
+    // Perna direita
+    ssd1306_line(&ssd, 64, 40, 68, 54, true); // dentro
+    ssd1306_line(&ssd, 70, 36, 76, 54, true); // fora
+    
+    ssd1306_line(&ssd, 68, 55, 70, 58, true); 
+    ssd1306_line(&ssd, 76, 55, 74, 58, true);
+
+    ssd1306_hline(&ssd, 71, 73, 58, true); // pé
+
+    // Atualiza o display
+    ssd1306_send_data(&ssd);
+}
